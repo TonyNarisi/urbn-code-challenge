d444321a11a56b7049bc869d3fa517f457f41b9b
Tony Narisi <tonynarisi@gmail.com>
2018-07-07 18:34:19 -0400

    installed igdb node sdk for server side api calls

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 const config = require('./webpack.config.js');
 const compiler = webpack(config);
 
+const igdb = require('igdb-api-node').default;
+const API_KEY = '8304f6f7976ed1399b55412a9acadeca';
+const client = igdb(API_KEY);
+
 app.use(webpackDevMiddleware(compiler, {
 	publicPath: config.output.publicPath
 }));
@@ -15,6 +19,17 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api', (req, res) => {
+	client.games({
+	    fields: '*', // Return all fields
+	    limit: 5, // Limit to 5 results
+	    offset: 15 // Index offset for results
+	}).then(response => {
+	    // response.body contains the parsed JSON response to this query
+	    console.log(response);
+	}).catch(error => {
+	    throw error;
+	});
+
 	res.json([{ 'hello': 'world' }]);
 })
 
